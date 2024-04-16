7c24e1cdf359b824fa0460394bf1f602423ddb7c
meddling-moose <ryanhoffman@Ryans-MacBook-Pro-13.local>
2024-04-15 19:04:11 -0700

    initial flask app and routes made

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import datetime as dt
+import numpy as np
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine('sqlite:///hawaii.sqlite')
+engine = create_engine('sqlite:///Resources/hawaii.sqlite')
 
 # reflect an existing database into a new model
 base = automap_base()
@@ -33,13 +34,6 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
-routes_dict = {'home': '/',
-               'precipitation': '/api/v1.0/precipitation',
-               'stations': '/api/v1.0/stations',
-               'tobs': '/api/v1.0/tobs',
-               'start': '/api/v1.0/<start>',
-               'startend': '/api/v1.0/<start>/<end>'}
-
 @app.route("/")
 def home():
     print("Server received request for 'Home' page...")
@@ -57,37 +51,71 @@ def home():
 def precipitation():
     print("Server received request for 'Precipitation' page...")
 
+    session = Session(engine)
+
     recent = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
     one_year_ago = dt.date(int(recent[0:4]), int(recent[5:7]), int(recent[8:10])) - dt.timedelta(days=365) #'2016-08-23'
 
-    # Perform a query to retrieve the data and precipitation scores
     data = session.query(Measurement.date, Measurement.prcp).\
-        filter(Measurement.date.between(one_year_ago, recent)).all()
+        filter(Measurement.date.between(one_year_ago, recent)).\
+        order_by(Measurement.date).all()
 
-    # Sort the dataframe by date
-    df = df.sort_values('date')
+    results = {date: prcp for date, prcp in data}
 
-    return "Welcome to my 'Precipitation' page"
+    session.close()
+    return jsonify(precipitation=results)
 
 @app.route("/api/v1.0/stations")
 def station():
     print("Server received request for 'Station' page...")
-    return "Welcome to my 'Station' page"
+    session = Session(engine)
+
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+
+    session.close()
+    return jsonify(stations=stations)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
     print("Server received request for 'Tobs' page...")
-    return "Welcome to my 'Tobs' page"
+
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date.between('2016-08-18', '2017-08-18')).all()
+
+    temperatures = list(np.ravel(results))
+
+    return jsonify(temperatures=temperatures)
 
 @app.route("/api/v1.0/<start>")
-def start():
-    print("Server received request for 'Start' page...")
-    return "Welcome to my 'Start' page"
+def start(start):
+    start = dt.datetime.strptime(start, '%m%d%Y')
+
+    print(f'date: {start}')
+
+    results = session.query(func.min(Measurement.tobs), 
+                            func.max(Measurement.tobs), 
+                            func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+    
+    stats = list(np.ravel(results))
+
+    return jsonify(minmaxavg=stats)
 
 @app.route("/api/v1.0/<start>/<end>")
-def startend():
-    print("Server received request for 'Start and End' page...")
-    return "Welcome to my 'Start and End' page"
+def startend(start, end):
+    start = dt.datetime.strptime(start, '%m%d%Y')
+    end = dt.datetime.strptime(end, '%m%d%Y')
+
+    results = session.query(func.min(Measurement.tobs), 
+                            func.max(Measurement.tobs), 
+                            func.avg(Measurement.tobs)).\
+        filter(Measurement.date.between(start, end)).all()
+
+    stats = list(np.ravel(results))
+
+    return jsonify(minmaxavg=stats)
 
 if __name__ == "__main__":
     app.run(debug=True)
